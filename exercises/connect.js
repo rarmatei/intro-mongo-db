7e19fa1427f427e5176ce472f6514cc96200cab7
Rares Matei <r.matei.10@aberdeen.ac.uk>
2019-12-05 19:12:58 +0000

    change to work with MongoAtlas

--- exercises/connect.js
+++ exercises/connect.js
@@ -1,8 +1,5 @@
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-
-const connect = (url) => mongoose.connect(url, {
-  useNewUrlParser: true
-})
-
-module.exports = connect
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const url = "mongodb+srv://<username>:<password>@cluster0-usofd.mongodb.net";
+const connect = db => mongoose.connect(`${url}/${db}`);
+module.exports = connect;
